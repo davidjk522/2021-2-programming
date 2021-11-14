0ed6347ed6033354f4459c17855ec3e182d432d1
davidjk522 <davidjk522@gmail.com>
2021-11-14 12:46:18 +0900

    2021-2프로그래밍 quiz4-2 제출용

--- 2021-2 quiz4-1 firstrial.c
+++ 2021-2 quiz4-1 firstrial.c
@@ -2,51 +2,71 @@
 #include <stdlib.h>
 #include <string.h>
 
-void addcount(double x, double y);
-void subcount(double x, double y);
+void addcount(double x, double y); //더하기 개수 세기
+void subcount(double x, double y); //뺴기 개수 세기
 
 int main()
 {
-	char input[100] = " "; //여유롭게 100칸
-	char operatior[] = { '+', '-', "end" }; // 연산자들
-	char number1[50], number2[50];
-	int opposition = 0;
-	double x = 0, y = 0;
+	char input[100] = " "; //여유롭게 100칸에 식을 받음
+
+	
 
 	printf("Enter \"end\" to end the program \n\n");
 
-	while (1) // 계속 반복
+	while (1) // end 입력하기 전까지 계속 반복
 	{
+		char number1[50] = { ' ' }, number2[50] = { ' ', }; //문자열로 입력 받은 식의 숫자들 추출하기
+		for (int i = 0; i < 50; i++)
+		{
+			number1[i] = ' ';
+			number2[i] = ' ';
+		}// 초기화 warning 피하기
+		int operator = 0; // 계산에 사용될 연산자의 위치(부호 아님)
+		double x = 0, y = 0; //atof 함수 이용해서 실수로 바꾼 값을 저장하기 위한 변시
 		printf("Enter arithematic expressions, including numbers and operators: ");
 		scanf_s("%s", input, 100);
-		if (strcmp( input,"end") == 0) // 문자열을 넣어서 strcmp==0 즉 end와 같으면 프로그램 종료 while 문 break하기
+
+		if (strcmp(input, "end") == 0) // 문자열을 넣어서 strcmp==0 즉 end와 같으면 프로그램 종료 while(1) 문 break하기
 		{
 			break;
 		}
-		else {
+		else { //end가 아닌 경우
 			for (int i = 0; i < 100; i++)
 			{
 
 				if (input[i] == '+')
 				{
-					opposition = i;
+					operator = i;
+					break;
 				}
 
 				else if (input[i] == '-')
 				{
-					opposition = i;
+					if (input[i] == input[i + 1])
+					{
+						operator = i;
+						break;
+					}
+					else
+					{
+						operator = i;
+					}
 				}
 
 			}//+-검사하는 for문 끝.. 처음에 -나+부호가 있더라도 중간에 있는 위치의 값으로 저장이 되어서 문제x
 
-			for (int i = 0; i < opposition; i++)
+			for (int i = 0; i < operator; i++)
 			{
-				number1[i] = input[i];
+				int temp;
+				temp = input[i];
+				number1[i] = temp;
 			}
 
 			for (int i = 0; i < 50; i++)
 			{
-				number2[i] = input[i + opposition + 1];
+				int temp;
+				temp = input[i + operator + 1];
+				number2[i] = temp;
 
 			}
 			x = atof(number1); //double로 변환
@@ -55,13 +75,13 @@ int main()
 
 
 			//더하기 뺴기, 지원하지 않은 연산 표시
-			if (input[opposition] == '+')
+			if (input[operator] == '+')
 			{
 				addcount(x, y);
 
 			}
 
-			else if (input[opposition] == '-')
+			else if (input[operator] == '-')
 			{
 				subcount(x, y);
 			}
@@ -70,9 +90,9 @@ int main()
 			{
 				printf("Not supported operation!\n");
 			}
-
+			
 		}// else, break에 해당하는 for 문
-		
+
 	}// while() 종료
 	printf("프로그램 종료");
 	return 0;
